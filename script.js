261687e93ce7b31a98a2a894234a7659028930e6
Deepak9912 <DEEPAKSINGHJ72@GMAIL.COM>
2023-05-06 17:53:20 +0000

    add logic to pay the game

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const tilesContainer = document.querySelector('.tiles');
 const colors = ["aqua", "aquamarine", "crimson", "blue", "dodgerblue", "gold", "greenyellow", "teal"];
-const colorsPicklIST = [...colors, ...colors];
+const colorsPicklIST = [...colors, ...colors]; // spread operator
 const titleCount = colorsPicklIST.length
 
 // Game state
@@ -13,9 +13,51 @@ function buildTiles(color){
     const element = document.createElement("div");
 
     element.classList.add("tile");
-    element.setAttribute("data-color", color)
+    element.setAttribute("data-color", color);
+    element.setAttribute("data-revealed", "false");
 
-    return element
+    //to display color when clicked
+    element.addEventListener('click', function(){
+        const revealed = element.getAttribute("data-revealed");
+
+        if(awaitingEndOfMove || revealed === 'true' || element === activeTile){
+            return;
+        }
+
+        element.style.backgroundColor = color;
+
+        if(!activeTile){
+            activeTile = element;
+            return;
+        }
+
+        const colotToMatch = activeTile.getAttribute("data-color");
+        if(colotToMatch === color){
+            activeTile.setAttribute("data-revealed", "true");
+            element.setAttribute("data-revealed", "true");
+
+            awaitingEndOfMove = false;
+            activeTile = null;
+            revealedCount = +2;
+
+            if (revealedCount === titleCount){
+                alert("Congrats! you won.")
+            }
+        }
+
+        awaitingEndOfMove = true;
+
+        setTimeout(function(){
+            element.style.backgroundColor = null;
+            activeTile.style.backgroundColor = null;
+
+            awaitingEndOfMove = false;
+            activeTile = null;
+        }, 1000);
+        
+    });
+
+    return element;
 }
 
 // Building tiles in dom
